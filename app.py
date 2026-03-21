c0de0fc95f4548d0e5fdb3a05b382bedabddf5e8
juanma3428-web <juanma3428@gmail.com>
2026-03-21 19:26:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,54 @@
 import streamlit as st
-import time
 
+# Configuración de página
 st.set_page_config(layout="centered", page_title="Trabajo")
 
-# Estilo para los botones grandes y el parpadeo
+# Estilo forzado para que se vea bien en iPhone
 st.markdown("""
     <style>
-    div.stButton > button {
-        height: 150px; width: 100%; font-size: 30px; border-radius: 15px;
-        color: white; font-weight: bold; margin-bottom: 10px;
+    .stButton > button {
+        height: 120px !important;
+        width: 100% !important;
+        font-size: 25px !important;
+        font-weight: bold !important;
+        color: white !important;
+        border-radius: 15px !important;
+        margin-bottom: 10px !important;
     }
-    /* Colores base */
-    .btn-parada { background-color: #0000FF; }
-    .btn-manilla { background-color: #008000; }
-    .btn-emisora { background-color: #FFA500; }
-    .btn-libre { background-color: #808080; }
+    /* Colores fijos */
+    div.stButton:nth-of-type(1) > button { background-color: #0047AB !important; } /* Azul */
+    div.stButton:nth-of-type(2) > button { background-color: #2E7D32 !important; } /* Verde */
     
-    /* El efecto al pulsar (Flash Verde) */
-    div.stButton > button:active { background-color: #00FF00 !important; }
+    /* Efecto parpadeo al tocar */
+    .stButton > button:active {
+        background-color: #00FF00 !important;
+        color: black !important;
+    }
     </style>
     """, unsafe_allow_html=True)
 
+st.title("Panel de Trabajo")
+
+# Botones Grandes
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button("PARADA", key="p"):
-        st.toast("Registrado: PARADA")
-        # Aquí es donde se conectará conmigo
-    if st.button("EMISORA", key="e"):
-        st.toast("Registrado: EMISORA")
+    if st.button("PARADA"):
+        st.toast("Registrando PARADA...")
+    if st.button("EMISORA"):
+        st.toast("Registrando EMISORA...")
 
 with col2:
-    if st.button("MANILLA", key="m"):
-        st.toast("Registrado: MANILLA")
-    if st.button("LIBRE", key="l"):
-        st.toast("Registrado: LIBRE")
+    if st.button("MANILLA"):
+        st.toast("Registrando MANILLA...")
+    if st.button("LIBRE"):
+        st.toast("Registrando LIBRE...")
 
 st.divider()
+
+# Botones Pequeños
 c1, c2, c3, c4 = st.columns(4)
-c1.button("FOTO")
-c2.button("MICRO")
-c3.button("CAFE")
-c4.button("FIN")
+with c1: st.button("FOTO")
+with c2: st.button("MIC")
+with c3: st.button("CAFE")
+with c4: st.button("FIN")
